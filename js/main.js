d26fdc739e071eef827913871df3cad9e03a8af7
Tiago <tiagoestevam7@hotmail.com>
2019-12-07 00:04:24 -0300

    add Js plugins

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 100,            // Integer: Speed of the transition, in milliseconds
+  timeout: 12000,          // Integer: Time between slide transitions, in milliseconds
+
+});
+
+
+$(".rslides_portfolio").responsiveSlides({
+
+  pager: true,           // Boolean:after: function () { }     // Function: After callback
+});
+
+
+
+
